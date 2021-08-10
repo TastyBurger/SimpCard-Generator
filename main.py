dd574e2c1c1a97ea9a26d3e26c385f3a2b1150ef
TastyBurger <88668860+TastyBurger@users.noreply.github.com>
2021-08-10 08:23:05 +0530

    Update main.py
    Updated the code to work with higher res base image

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PIL import Image
-rdim = (201,308)
+rdim = (249,380)
 Which_file = str(input('Enter the location of the image(Make sure the aspect ratio of the file is close to 2x3): '))
 baseimage = Image.open('bfile.png')
 in_img = Image.open(Which_file)
 newin_img = in_img.resize(rdim)
-baseimage.paste(newin_img,(38,64))
+baseimage.paste(newin_img,(46,78))
 baseimage.save('Output.png')
