a1e6781fad6e7607b62aba4abc435138fcb4b2cf
TastyBurger <88668860+TastyBurger@users.noreply.github.com>
2021-08-12 11:09:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PIL import Image
 rdim = (249,380)
 Which_file = str(input('Enter the location of the image(Make sure the aspect ratio of the file is close to 2x3): '))
-baseimage = Image.open(Which_file)
-in_img = Image.open('iamge2.jpg')
+baseimage = Image.open('bfile.png')
+in_img = Image.open(Which_file)
 width, height = in_img.size
 print(width)
 print(height)
